63bdfa27cb09ed0fe54fc82af838e4ce97cf0e75
Thora Hornburg <thora.hornburg@gmx.de>
2021-07-26 15:39:41 +0200

    init css

--- statamic/webpack.mix.js
+++ statamic/webpack.mix.js
@@ -12,9 +12,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/site.js', 'public/js')
 
-mix.postCss('resources/css/tailwind.css', 'public/css', [
+mix.postCss('resources/css/main.css', 'public/css', [
     require('postcss-import'),
-    require('tailwindcss'),
     require('postcss-nested'),
     require('postcss-preset-env')({stage: 0})
 ])

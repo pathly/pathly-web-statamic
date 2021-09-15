3ffcb64a21a0bf4cde8b95747b07e9ca00e959be
Doc-debug <jan.naubert@googlemail.com>
2021-09-15 17:04:02 +0200

    added mobile menu functionality, added jquery + site.js

--- statamic/resources/js/site.js
+++ statamic/resources/js/site.js
@@ -1,1 +1,24 @@
 // This is all you.
+jQuery(() => {
+    initMenuListeners();
+});
+
+// menu mobile buttons
+function initMenuListeners() {
+    // open and close mobile menu
+    $(".header-nav-switch-mobile").on("click", function () {
+        $(this).parent().toggleClass("active");
+    });
+
+    // move to sub menu
+    $(".header-nav-list .is-parent").on("click", function () {
+        $(".header-nav-list .is-parent.active").removeClass("active");
+        $(this).toggleClass("active");
+        $("#header-nav").addClass("sub-menu-active");
+    });
+
+    // move back from sub menu to main menu part
+    $( document ).on("click" , ".header-nav-sub-back", function(){
+        $("#header-nav").removeClass("sub-menu-active");
+    });
+}

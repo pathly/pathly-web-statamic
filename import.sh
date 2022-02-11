ebc0ce0c1afa7b601272047214c3472f95df2ddf
Patrick Baber <patrick@p-squared.de>
2022-02-11 15:48:58 +0100

    exclude some files and folders from import

--- import.sh
+++ import.sh
@@ -5,6 +5,11 @@ SSH_PORT="22701"
 SSH_USER="sshuser_zeibzc"
 SSH_PATH="/var/www/releases/current/"
 
-rsync -avz -e "ssh -p $SSH_PORT" $SSH_USER@$SSH_HOST:$SSH_PATH statamic/
+rsync -avz -e "ssh -p $SSH_PORT" \
+    --exclude=".env" \
+    --exclude="vendor/" \
+    --exclude="node_modules/" \
+    --exclude="package-lock.json" \
+    $SSH_USER@$SSH_HOST:$SSH_PATH statamic/
 
 echo "Done"
